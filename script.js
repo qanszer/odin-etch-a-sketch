f2217c504c97133b9a6b202d61636a1bdd9e69bd
qanszer <maluendajohnsteven@gmail.com>
2026-05-26 00:23:15 +0800

    feat: add the canvas

---
+++ script.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector('.canvas');
+
+let numberOfSquares = 256;
+
+for (let i = 1; i <= numberOfSquares; i++) {
+    console.log("a");
+    let square = document.createElement('div');
+    square.classList.add('square');
+    canvas.appendChild(square);
+}
